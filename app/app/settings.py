4ed720ba7afa4bbd90b855745bc92caa6db7c4ac
Andrey Ivanov <ivnv.xd@gmail.com>
2023-11-22 22:57:49 +0100

    Add drf_spectacular

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
+    "rest_framework",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,7 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "core.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
