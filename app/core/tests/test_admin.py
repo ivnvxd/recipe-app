718ec633baf3b242fce18e73401ca4e6e2f95694
Andrey Ivanov <ivnv.xd@gmail.com>
2023-11-22 22:18:52 +0100

    Add user adding admin page

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -35,3 +35,10 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works."""
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
