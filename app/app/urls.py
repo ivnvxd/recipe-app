72d2ba696d5cef1353c97ba384627c25554408ac
Andrey Ivanov <ivnv.xd@gmail.com>
2023-11-23 15:25:52 +0100

    Add user API

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -26,4 +26,5 @@ urlpatterns = [
     path(
         "api/docs/", SpectacularSwaggerView.as_view(url_name="api-schema"), name="docs"
     ),
+    path("api/user/", include("user.urls")),
 ]
