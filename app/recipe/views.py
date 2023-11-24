7b9aace3ff2cdef7ce40690a99c87dfc2866a466
Andrey Ivanov <ivnv.xd@gmail.com>
2023-11-24 16:41:18 +0100

    Add recipe API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -27,3 +27,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return serializers.RecipeSerializer
 
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
