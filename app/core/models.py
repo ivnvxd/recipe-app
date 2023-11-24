f8496ee30d9d840f954c19d6b1a265070cc6fc87
Andrey Ivanov <ivnv.xd@gmail.com>
2023-11-24 13:37:56 +0100

    Add recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 """
 Database models
 """
+from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
@@ -44,3 +45,21 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+
+
+class Recipe(models.Model):
+    """Recipe object."""
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        """Return string representation of the recipe."""
+        return self.title
