dac49d5f4d17820d8fa14ae2325b08d052142838
Andrey Ivanov <ivnv.xd@gmail.com>
2023-11-25 16:58:03 +0100

    Add tests for tag model

--- app/core/models.py
+++ app/core/models.py
@@ -59,7 +59,22 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField("Tag")
 
     def __str__(self):
         """Return string representation of the recipe."""
         return self.title
+
+
+class Tag(models.Model):
+    """Tag for filtering recipes."""
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        """Return string representation of the tag."""
+        return self.name
