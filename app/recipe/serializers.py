824dea18063b6dab401ac92e6d1948e5266615b9
Andrey Ivanov <ivnv.xd@gmail.com>
2023-11-25 18:35:42 +0100

    Add updating tags feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -32,10 +32,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         )
         read_only_fields = ["id"]
 
-    def create(self, validated_data):
-        """Create a new recipe."""
-        tags = validated_data.pop("tags", [])
-        recipe = Recipe.objects.create(**validated_data)
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed."""
         auth_user = self.context["request"].user
         for tag in tags:
             tag_obj, _ = Tag.objects.get_or_create(
@@ -44,7 +42,26 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def create(self, validated_data):
+        """Create a new recipe."""
+        tags = validated_data.pop("tags", [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
+
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update a recipe."""
+        tags = validated_data.pop("tags", None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
