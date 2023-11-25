4318315d1989050ba73cf5c8e9066d807c1b282d
Andrey Ivanov <ivnv.xd@gmail.com>
2023-11-25 17:30:03 +0100

    Add tag API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -2,7 +2,7 @@
 Serializers for recipe APIs
 """
 
-from core.models import Recipe
+from core.models import Recipe, Tag
 from rest_framework import serializers
 
 
@@ -26,3 +26,12 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ("description",)
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ("id", "name")
+        read_only_fields = ["id"]
--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -8,6 +8,7 @@ from recipe import views
 
 router = DefaultRouter()
 router.register("recipes", views.RecipeViewSet)
+router.register("tags", views.TagViewSet)
 
 app_name = "recipe"
 
