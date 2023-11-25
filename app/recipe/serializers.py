07ebaf6b23e53f984c82dadcc72d4d0f7a7cb5b8
Andrey Ivanov <ivnv.xd@gmail.com>
2023-11-25 18:19:22 +0100

    Add creating tags feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -6,8 +6,19 @@ from core.models import Recipe, Tag
 from rest_framework import serializers
 
 
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ("id", "name")
+        read_only_fields = ["id"]
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
+
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
@@ -17,8 +28,23 @@ class RecipeSerializer(serializers.ModelSerializer):
             "time_minutes",
             "price",
             "link",
+            "tags",
         )
         read_only_fields = ["id"]
+
+    def create(self, validated_data):
+        """Create a new recipe."""
+        tags = validated_data.pop("tags", [])
+        recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context["request"].user
+        for tag in tags:
+            tag_obj, _ = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
+
+        return recipe
 
 
 class RecipeDetailSerializer(RecipeSerializer):
@@ -26,12 +52,3 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ("description",)
-
-
-class TagSerializer(serializers.ModelSerializer):
-    """Serializer for tags."""
-
-    class Meta:
-        model = Tag
-        fields = ("id", "name")
-        read_only_fields = ["id"]
