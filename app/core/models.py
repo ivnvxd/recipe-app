6349e2f420827d2a4077c24ad72998dec6c7f549
Andrey Ivanov <ivnv.xd@gmail.com>
2023-11-26 11:41:03 +0100

    Add ingredient model

--- app/core/models.py
+++ app/core/models.py
@@ -60,6 +60,7 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField("Tag")
+    ingredients = models.ManyToManyField("Ingredient")
 
     def __str__(self):
         """Return string representation of the recipe."""
@@ -77,4 +78,18 @@ class Tag(models.Model):
 
     def __str__(self):
         """Return string representation of the tag."""
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient for recipes."""
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        """Return string representation of the ingredient."""
         return self.name
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -81,3 +81,13 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        """Test creating an ingredient is successful."""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name="Ingredient1",
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
