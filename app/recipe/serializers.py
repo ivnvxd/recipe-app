7975e6b959dd61073c224f2ddd5b017a8ef6e7b5
Andrey Ivanov <ivnv.xd@gmail.com>
2023-11-27 12:35:51 +0100

    Add image upload feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -89,3 +89,13 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ("description",)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ("id", "image")
+        read_only_fields = ["id"]
+        extra_kwargs = {"image": {"required": True}}
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,9 +2,11 @@
 Views for the recipe APIs.
 """
 from core.models import Ingredient, Recipe, Tag
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, status, viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from recipe import serializers
 
@@ -25,12 +27,35 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return the serializer class for request."""
         if self.action == "list":
             return serializers.RecipeSerializer
+        elif self.action == "upload_image":
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+    @action(methods=["POST"], detail=True, url_path="upload-image")
+    def upload_image(self, request, pk=None):
+        """Upload an image to a recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(
+            recipe,
+            data=request.data,
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK,
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 
 
 class BaseRecipeAttrViewSet(
