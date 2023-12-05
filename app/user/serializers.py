f53891a668bca65fca352c356273e90ad72c9710
Andrey Ivanov <ivnv.xd@gmail.com>
2023-12-05 14:40:40 +0100

    Upgrade django

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth import (
     authenticate,
     get_user_model,
 )
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 
